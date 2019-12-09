41c73652cd453f0b65bc830ee425fe8cf2c03600
Dominika Miodowska <dominika.miodowska@ongoing.ch>
2019-12-09 08:26:36 +0100

    add comments

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,14 +1,14 @@
 'use strict'
 /* Global Variables */
-//Personal API Key for Pixabay
+//Personal API Key for Pixabay  (Photo)
 const pixabayApiKey = '?key=14429196-984aeaa78fd5e3a738036f230'
 const pixabayBaseURL = `https://pixabay.com/api/${pixabayApiKey}&q=`
 
-//Personal API Key for Dark Blue
+//Personal API Key for Dark Blue  (weather)
 const darkApiKey = 'ae6ef7c8ff0231dd4f01ee4108e6413b'
 const darkBaseURL = `https://api.darksky.net/forecast/${darkApiKey}/`
 
-// Personal API Key for GEONAMES API
+// Personal API Key for GEONAMES API  (location, lat, low)
 const apiKey = 'username=dominika_ongoing';
 const baseURL = `http://api.geonames.org/searchJSON?${apiKey}&`;
 
@@ -62,24 +62,24 @@ const updateUI = async (newData) => {
   
   // Create a new date instance dynamically with JS
   var x = Math.floor((Math.random() * 25) + 1);   //day
-  var y = Math.floor((Math.random() * 9) );   //month
-  var s = Math.floor((Math.random() * 5) +1);   //year
+  var y = Math.floor((Math.random() * 9) +1);   //month
+  var s = Math.floor((Math.random() * 5)+1);   //year
 
   // DEPARTING TIME 
   let t = new Date();
-  let newDateTripX = new Date( (t.getMonth()-y)+'/'+ (t.getDate()+ x) + '/' +(t.getFullYear()+s))
+  let newDateTrip = new Date( (t.getMonth()-y)+'/'+ (t.getDate()+ x) + '/' +(t.getFullYear()+s))
     
   // Today Date
   let d = new Date();
-  let newDateX = new Date((d.getMonth()+1)+'/'+ d.getDate()+'/'+ d.getFullYear());
+  let newDate = new Date((d.getMonth()+1)+'/'+ d.getDate()+'/'+ d.getFullYear());
 
   //Calculate the difference in Time and in Days
-  let differencie_in_time = (newDateTripX.getTime())-(newDateX.getTime());
+  let differencie_in_time = (newDateTrip.getTime())-(newDate.getTime());
   let difference_in_days = Math.floor(differencie_in_time/(1000*3600*24));
   
   //#########################   update the UI    ############################################
-  document.getElementById('date').innerHTML = `<h3>Today is: ${newDateX.getDate()+'.'+(newDateX.getMonth()+1)+'.'+ newDateX.getFullYear()}</h3>`;
-  document.getElementById('trips-time').innerHTML = `<h3>Departing: ${ newDateTripX.getDate()+'.'+newDateTripX.getMonth()+'.'+ newDateTripX.getFullYear()}</h3>`;  
+  document.getElementById('date').innerHTML = `<h3>Today is: ${newDate.getDate()+'.'+(newDate.getMonth()+1)+'.'+ newDate.getFullYear()}</h3>`;
+  document.getElementById('trips-time').innerHTML = `<h3>Departing: ${ newDateTrip.getDate()+'.'+newDateTrip.getMonth()+'.'+ newDateTrip.getFullYear()}</h3>`;  
   document.getElementById('days').innerHTML = `${difference_in_days}`
 
   document.getElementsByClassName('country')[0].innerHTML = newData[0].name;
